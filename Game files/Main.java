f509c1d1f4e01e99ec43addf96e6cd84e13a0b79
ShreyMarwaha <55681068+ShreyMarwaha@users.noreply.github.com>
2020-11-15 17:46:28 +0530

    Update Main.java

--- Game files/Main.java
+++ Game files/Main.java
@@ -1,11 +1,15 @@
 package sample;
 
+import com.sun.javafx.scene.SceneEventDispatcher;
 import javafx.application.*;
+import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 
 import javafx.stage.*;
+
+import java.io.IOException;
 
 public class Main extends Application {
     public static void main(String[] args) {launch(args);}
@@ -14,17 +18,16 @@ public class Main extends Application {
 //    Scene Scene1, Scene2;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu_front.fxml"));
         Window = primaryStage;
-        Window.setOnCloseRequest(e-> { e.consume();  Quit();}   );
         Window.setScene(new Scene(root));
         Window.setTitle("Color Switch | Shrey Marwaha | Arnav Gupta");
         Window.show();
-
     }
 
     public void NewGame(){
 //        TODO: method to START New Game
+        Game game = new Game();
         System.out.println("new Game started");
 
     }
@@ -35,11 +38,16 @@ public class Main extends Application {
 
     }
 
+    @FXML
+    public Button LoadMenuButton;
 //    ArrayList<> SavedGames = new ArrayList<>();
-    public void LoadGame(){
+    public void LoadGame() throws IOException {
 //        TODO: method for laoding game from arraylist
+        Parent root = FXMLLoader.load(getClass().getResource("Menu_LoadGame.fxml"));
+        Scene LoadMenu = new Scene(root);
+        Stage stage = (Stage) LoadMenuButton.getScene().getWindow();
+        stage.setScene(LoadMenu);
     }
-
 
     @FXML
     public Button closeButton;
